ea2a4e2d96e490caef1791db2d22675319f70441
FadelJoan <150167377+FadelJoan@users.noreply.github.com>
2023-11-07 23:11:40 +0700

    Add files via upload

---
+++ ddptabel.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,j; 
+	int n = 3;
+ 	for (i=3; i<=n; i++) { 
+ 		for (j=1; j<=i; j++) { 
+ 	cout<<"----------------" << endl;
+	cout<<"|    |    |    |" << endl;
+	}
+ 	cout<<"----------------" << endl;
+ 	} 
+		
+	 return 0;
+}
+
